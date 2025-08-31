91f6523ed5e8bb38de720354390cf33825a2214b
Anas Shaikh <anas.jshaikh04@gmail.com>
2025-08-31 15:38:59 +0530

    file not found error solve

--- haga/sentiment/views.py
+++ haga/sentiment/views.py
@@ -7,37 +7,37 @@ from ml_model import visualizations as viz
 
 from ml_model.sentiment_model import senty_pred
 
-#def ha(request):
-#    return render(request, "index.html")
-
 def ha(request):
-    df = pd.read_csv(r"D:\hagothon\haga\ml_model\500.csv")
-    df['Sentiment'] = df['Sentiment'].fillna('No Data')
+   return render(request, "index.html")
 
-    threat_keywords = [
-        'attack', 'murder', 'rape', 'assault', 'kidnap', 'violence', 
-        'bomb', 'shoot', 'stab', 'robbery', 'terror', 'threat', 'kill'
-        ]
+# def ha(request):
+#     df = pd.read_csv(r"D:\hagothon\haga\ml_model\500.csv")
+#     df['Sentiment'] = df['Sentiment'].fillna('No Data')
 
-    df['Threat'] = df['Text'].apply(lambda x: 1 if any(word in str(x).lower() for word in threat_keywords) else 0)
+#     threat_keywords = [
+#         'attack', 'murder', 'rape', 'assault', 'kidnap', 'violence', 
+#         'bomb', 'shoot', 'stab', 'robbery', 'terror', 'threat', 'kill'
+#         ]
+
+#     df['Threat'] = df['Text'].apply(lambda x: 1 if any(word in str(x).lower() for word in threat_keywords) else 0)
     
-    total_posts = len(df)
-    threats_detected = df['Threat'].sum()
-    overall_sentiment = df['Sentiment'].mode().iloc[0] if not df.empty else "No Data"
+#     total_posts = len(df)
+#     threats_detected = df['Threat'].sum()
+#     overall_sentiment = df['Sentiment'].mode().iloc[0] if not df.empty else "No Data"
 
-    keyword_counts = {}
-    for kw in threat_keywords:
-        keyword_counts[kw] = df['Text'].str.lower().str.contains(kw).sum()
+#     keyword_counts = {}
+#     for kw in threat_keywords:
+#         keyword_counts[kw] = df['Text'].str.lower().str.contains(kw).sum()
 
-    context = {
-    "total_posts": total_posts,
-    "threats_detected": threats_detected,
-    "overall_sentiment": overall_sentiment,
-    "threat_keywords": threat_keywords, 
-    "df": df, 
-    "keyword_counts": keyword_counts,
-    }
-    return render(request, "index.html", context)
+#     context = {
+#     "total_posts": total_posts,
+#     "threats_detected": threats_detected,
+#     "overall_sentiment": overall_sentiment,
+#     "threat_keywords": threat_keywords, 
+#     "df": df, 
+#     "keyword_counts": keyword_counts,
+#     }
+#     return render(request, "index.html", context)
 
 def model(request):
     prediction = None
