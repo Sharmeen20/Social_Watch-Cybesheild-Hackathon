2ff6f13afc2a6bbca2e71b7845a6f99025680f15
Sushil Dipak Chavan <sushilchavan2468@gmail.com>
2025-09-01 18:18:30 +0530

    Update settings.py

--- haga/haga/settings.py
+++ haga/haga/settings.py
@@ -17,7 +17,9 @@ import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,192.168.1.5').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+
+#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,192.168.1.5').split(',')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
