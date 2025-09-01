04b71e5b7488f9695677edb11818e59d127eb852
Sushil Dipak Chavan <sushilchavan2468@gmail.com>
2025-09-01 12:45:18 +0530

    Update settings.py

--- haga/haga/settings.py
+++ haga/haga/settings.py
@@ -11,12 +11,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+'''
 # Environment variables for deployment
 # ---------------------------
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+'''
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-v56im!&p9you_%=!%kc%kh42ykfqe-fmj6*gkdm$4f)0q2k3gk'
+SECRET_KEY = 'django-insecure-v56im!&p9you_%=!%kc%kh42ykfqe-fmj6*gkdm$4f)0q2k3gk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = False
 
-#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['social-watch.onrender.com']
 
 
 # Application definition
